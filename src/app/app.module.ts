9e06647b7c8ff08bc1ca6e3b644a3ed37d11c871
sonu kumar <souravk03221@outlook.com>
2025-02-26 23:30:05 +0530

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { HeaderComponent } from "./header/header.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 
 
 @NgModule({
-    declarations:[AppComponent],
+    declarations:[AppComponent,HeaderComponent,RecipesComponent,
+    RecipeDetailComponent,RecipeItemComponent,RecipeListComponent,ShoppingListComponent,ShoppingEditComponent],
     imports:[BrowserModule],
     bootstrap:[AppComponent]
 })
