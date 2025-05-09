f630873efb4fc1483d29e58e6a22b09d9d1bf356
sonu kumar <souravk03221@outlook.com>
2025-05-09 23:48:59 +0530

    modified auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,9 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 interface AuthResponse{
+    code:string;
+    message:string;
+    status:string;
     token:string;
 }
 @Injectable({providedIn:'root'})
@@ -9,7 +12,7 @@ export class AuthService{
     constructor(private http: HttpClient){}
 
     signup(email:string, password: string){
-        return this.http.post<AuthResponse>('',
+        return this.http.post<AuthResponse>('http://localhost:8080/createUser',
         {
             email:email,
             password:password
