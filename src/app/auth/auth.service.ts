bf816651b36094f536505b0f562597c1687c47f0
sonu kumar <souravk03221@outlook.com>
2025-05-10 21:12:38 +0530

    added login function

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { catchError, throwError } from "rxjs";
 
-interface AuthResponse{
+export interface AuthResponse{
     code:string;
     message:string;
     status:string;
@@ -16,6 +17,35 @@ export class AuthService{
         {
             email:email,
             password:password
-        });
+        }
+        ).pipe(catchError(this.handleError));
     }
+
+    login(email:string, password:string){
+        return this.http.post<AuthResponse>('',
+        {
+            email:email,
+            password:password
+        }).pipe(catchError(this.handleError));;
+    }
+
+    private handleError(errorRes: HttpErrorResponse){
+        let errorMessage ='An unknown error occured! ';
+        if(!errorRes.error){
+            return throwError(errorMessage);
+        }
+        switch(errorRes.error){
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'this email does not exits.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct';
+                break;
+        }
+        return throwError(errorMessage);
+    }
+    
 }
