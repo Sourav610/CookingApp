e5114ad8cc856a78b07e4d61b7fd3648bf651619
sonu kumar <souravk03221@outlook.com>
2025-05-25 23:06:38 +0530

    segregate different module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { HeaderComponent } from "./header/header.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { CommonModule } from "@angular/common";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { RouterAppComponent } from "./app.route";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RecipeService } from "./recipes/recipe.service";
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
@@ -21,17 +16,18 @@ import { LoadingSpinnerComponent } from "./shared/Loading-spinner/Loading-spinee
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { AlertComponent } from "./shared/alert/alert.component";
 import { PlaceholderDirective } from "./shared/placeholder/placeholder.directive";
+import { RecipesModule } from "./recipes/recipes.module";
 
 
 @NgModule({
-    declarations:[AppComponent,HeaderComponent,RecipesComponent,
-    RecipeDetailComponent,RecipeItemComponent,RecipeListComponent,ShoppingListComponent,
-    ShoppingEditComponent,DropdownDirective,RecipeEditComponent
+    declarations:[AppComponent,HeaderComponent,ShoppingListComponent,
+    ShoppingEditComponent,DropdownDirective
     ,AuthComponent,LoadingSpinnerComponent,
     AlertComponent,PlaceholderDirective],
     imports:[BrowserModule,CommonModule,
         RouterAppComponent,
         ReactiveFormsModule,
+        RecipesModule,
     FormsModule,HttpClientModule],
     providers:[ShoppingListService,RecipeService,{provide: HTTP_INTERCEPTORS,useClass:AuthInterceptorService,multi:true}],
     bootstrap:[AppComponent]
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipesComponent } from "./recipes.component";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+    declarations:[
+        RecipesComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeListComponent,
+        RecipeEditComponent
+    ],
+    imports:[RouterModule,CommonModule,ReactiveFormsModule],
+    exports:[
+        RecipesComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeListComponent,
+        RecipeEditComponent
+    ]
+})
+export class RecipesModule{}
